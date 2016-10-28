fe6a3b8a62f7c37a84e87c04fdc4ddebcd528c54
Cesar Brenes <cesarbresola@gmail.com>
2016-10-28 14:21:18 -0600

    added initial project

---
+++ PurchaseList/PurchaseListTests/PurchaseListTests.swift
@@ -0,0 +1,36 @@
+//
+//  PurchaseListTests.swift
+//  PurchaseListTests
+//
+//  Created by Cesar Brenes on 10/28/16.
+//  Copyright © 2016 Cesar Brenes. All rights reserved.
+//
+
+import XCTest
+@testable import PurchaseList
+
+class PurchaseListTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
